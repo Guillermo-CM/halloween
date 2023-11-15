bf9af3b29854a981e817741cbd9d980792e97c90
Guillermo-CM <Guillermo.castm@hotmail.com>
2023-11-14 21:47:19 -0800

    feature: "add tuto, infinite, ui_controls screens"

--- lib/config/router/list_router.dart
+++ lib/config/router/list_router.dart
@@ -1,9 +1,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:halloween/screens/animated/animated_screen.dart';
+import 'package:halloween/screens/app_tutorial/app_tutorial_screen.dart';
+import 'package:halloween/screens/infinite_scroll/infinite_scroll_screen.dart';
 import 'package:halloween/screens/progress/progress_screen.dart';
 import 'package:halloween/screens/screens.dart';
 import 'package:halloween/screens/snackbar/snackbar_screen.dart';
+import 'package:halloween/screens/ui_controls/ui_controls_screen.dart';
 
 
 final List<RouteBase> routes = [
@@ -31,5 +35,26 @@ final List<RouteBase> routes = [
     path: '/snackbars',
     name: SnackBarScreen.screenName,
     builder: (BuildContext context, GoRouterState state) => const SnackBarScreen(),
+  ),
+    GoRoute(
+    path: '/animated',
+    name: AnimatedScreen.screenName,
+    builder: (BuildContext context, GoRouterState state) =>
+        const AnimatedScreen(),
+  ),
+  GoRoute(
+    path: '/ui-controls',
+    name: UiControlsScreen.screenName,
+    builder: (context, state) => const UiControlsScreen(),
+  ),
+  GoRoute(
+    path: '/tutorial',
+    name: AppTutorialScreen.screenName,
+    builder: (context, state) => const AppTutorialScreen(),
+  ),
+  GoRoute(
+    path: '/infinite',
+    name: InfiniteScrollScreen.screenName,
+    builder: (context, state) => const InfiniteScrollScreen(),
   ),
 ];
